2e073747535a91f7526270bb5943218f42dc1935
Marcelo Schmidt <marcelo.schmidt@gmail.com>
2021-01-07 15:56:09 -0300

    Fix holidays schedules

--- actions/Whosout.ts
+++ actions/Whosout.ts
@@ -1,4 +1,5 @@
 import { IHttp, IModify, IPersistence, IRead } from '@rocket.chat/apps-engine/definition/accessors';
+import { IMessageAttachment } from '@rocket.chat/apps-engine/definition/messages';
 import { IMessageAttachmentField } from '@rocket.chat/apps-engine/definition/messages/IMessageAttachmentField';
 import { IUser } from '@rocket.chat/apps-engine/definition/users';
 
@@ -32,7 +33,8 @@ export class Whosout {
             next = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
         }
 
-        const fields: Array<IMessageAttachmentField> = [];
+        const attachments: Array<IMessageAttachment> = [];
+        let fields: Array<IMessageAttachmentField> = [];
         const outToday: Array<string> = [];
         const outNext: Array<string> = [];
         for (const leave of result.data) {
@@ -40,13 +42,13 @@ export class Whosout {
                 const from = new Date(leave.From);
                 const to = new Date(leave.To);
                 const amount = leave['Days/Hours Taken'];
-                let info = `, ${amount}, until ${leave.To}`;
+                let info = `, ${amount.replace('.0', '')} ${leave.Unit.toLowerCase()}${parseInt(amount) > 1 ? 's' : ''}${leave.Unit === 'Hour' ? '' : `, until ${leave.To}`}`;
 
                 if (leave.ApprovalStatus === 'Pending') {
                     info += ' _(pending)_';
                 }
 
-                const who = `*${leave.ownerName}*${info}`;
+                const who = `${leave.ownerName}${info}`;
                 if (isDateBetween(today, from, to)) {
                     outToday.push(who);
                 } else if (isDateBetween(next, from, to)) {
@@ -71,76 +73,104 @@ export class Whosout {
             });
         }
 
-        const holidaysToday: Array<string> = [];
-        const holidaysNext: Array<string> = [];
-        const holidaysWeekend: Array<string> = [];
-        const holidaysNextWeekend: Array<string> = [];
-        const countryList = this.app.holidayCountries.split(',');
-        for (const country of countryList) {
-            const urlHoliday = `https://date.nager.at/api/v2/publicholidays/${ today.getFullYear() }/${ country }`;
-            const holidayResult = await http.get(urlHoliday);
-            if (holidayResult && holidayResult.content && holidayResult.content.length > 0) {
-                for (const holiday of holidayResult.data) {
-                    const date = new Date(holiday.date + 'T00:00:00').toISOString();
-                    const name = holiday.localName + (holiday.countryCode === 'US' ? '' : ` (${ holiday.name })`);
-                    if (date === today.toISOString()) {
-                        holidaysToday.push(`${ holiday.countryCode } - ${ name }`);
-                    } else if (date === next.toISOString()) {
-                        holidaysNext.push(`${holiday.countryCode} - ${name}`);
-                    } else if (today.getDay() === 1) {
-                        const saturday = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 2).toISOString();
-                        const sunday = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 1).toISOString();
-                        if (date === saturday) {
-                            holidaysWeekend.push(`Saturday, ${holiday.countryCode} - ${name}`);
-                        } else if (date === sunday) {
-                            holidaysWeekend.push(`Sunday, ${holiday.countryCode} - ${name}`);
-                        }
-                    } else if (today.getDay() === 5) {
-                        const saturday = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1).toISOString();
-                        const sunday = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 2).toISOString();
-                        if (date === saturday) {
-                            holidaysNextWeekend.push(`Saturday, ${holiday.countryCode} - ${name}`);
-                        } else if (date === sunday) {
-                            holidaysNextWeekend.push(`Sunday, ${holiday.countryCode} - ${name}`);
+        if (fields.length > 0) {
+            attachments.push({ fields, color: 'red' });
+            fields = [];
+        }
+
+        const locations = {};
+        const urlUsers = `https://people.zoho.com/people/api/forms/P_EmployeeView/records?authtoken=${this.app.peopleToken}`;
+        const resultUsers = await http.get(urlUsers);
+        if (resultUsers && resultUsers.content && resultUsers.content.length > 0) {
+            for (const person of resultUsers.data) {
+                if (person['Location Name']) {
+                    const username = person.ownerName;
+                    if (locations[person['Location Name']]) {
+                        locations[person['Location Name']].people.push(username);
+                    } else {
+                        locations[person['Location Name']] = { people: [ username ], holidayToday: [], holidayNext: [], holidayWeekend: [], holidayNextWeekend: [] };
+                        const urlHoliday = `https://people.zoho.com/people/api/leave/getHolidays?authtoken=${this.app.peopleToken}&userId=${person.recordId}`;
+                        const resultHoliday = await http.get(urlHoliday);
+                        if (resultHoliday && resultHoliday.data && resultHoliday.data.response && resultHoliday.data.response.result) {
+                            for (const holiday of resultHoliday.data.response.result) {
+                                const date = new Date(holiday.fromDate + 'T00:00:00').toISOString();
+                                const name = holiday.Name.substring(holiday.Name.lastIndexOf(':') + 1);
+                                if (date === today.toISOString()) {
+                                    locations[person['Location Name']].holidayToday.push(name);
+                                } else if (date === next.toISOString()) {
+                                    locations[person['Location Name']].holidayNext.push(name);
+                                } else if (today.getDay() === 1) {
+                                    const saturday = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 2).toISOString();
+                                    const sunday = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 1).toISOString();
+                                    if (date === saturday) {
+                                        locations[person['Location Name']].holidayWeekend.push(`Saturday, ${ name }`);
+                                    } else if (date === sunday) {
+                                        locations[person['Location Name']].holidayWeekend.push(`Sunday, ${name}`);
+                                    }
+                                } else if (today.getDay() === 5) {
+                                    const saturday = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1).toISOString();
+                                    const sunday = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 2).toISOString();
+                                    if (date === saturday) {
+                                        locations[person['Location Name']].holidayNextWeekend.push(`Saturday, ${name}`);
+                                    } else if (date === sunday) {
+                                        locations[person['Location Name']].holidayNextWeekend.push(`Sunday, ${name}`);
+                                    }
+                                }
+                            }
                         }
                     }
                 }
             }
         }
 
-        if (holidaysToday.length > 0) {
-            fields.push({
-                title: `Holidays today:`,
-                value: holidaysToday.join('\n'),
-                short: true,
-            });
+        if (Object.keys(locations).length > 0) {
+            for (const location of Object.keys(locations)) {
+                if (locations[location].holidayToday.length > 0) {
+                    fields.push({
+                        title: `Holiday today in ${location}:`,
+                        value: `${locations[location].holidayToday.join('\n')}\n\n*People in ${location}:*\n${locations[location].people.join('\n')}`,
+                        short: true,
+                    });
+                }
+
+                if (locations[location].holidayNext.length > 0) {
+                    fields.push({
+                        title: `Holiday ${ monday ? 'on Monday': 'tomorrow' } in ${location}:`,
+                        value: `${locations[location].holidayNext.join('\n')}\n\n*People in ${location}:*\n${locations[location].people.join('\n')}`,
+                        short: true,
+                    });
+                }
+
+                if (fields.length > 0) {
+                    attachments.push({ fields, color: 'orange' });
+                    fields = [];
+                }
+
+                if (locations[location].holidayWeekend.length > 0) {
+                    fields.push({
+                        title: `Holiday past weekend in ${location}:`,
+                        value: `${locations[location].holidayWeekend.join('\n')}\n\n*People in ${location}:*\n${locations[location].people.join('\n')}`,
+                        short: true,
+                    });
+                }
+
+                if (locations[location].holidayNextWeekend.length > 0) {
+                    fields.push({
+                        title: `Holiday next weekend in ${location}:`,
+                        value: `${locations[location].holidayNextWeekend.join('\n')}\n\n*People in ${location}:*\n${locations[location].people.join('\n')}`,
+                        short: true,
+                    });
+                }
+
+
+                if (fields.length > 0) {
+                    attachments.push({ fields, color: 'orange' });
+                    fields = [];
+                }
+            }
         }
 
-        if (holidaysWeekend.length > 0) {
-            fields.push({
-                title: `Holidays past weekend:`,
-                value: holidaysWeekend.join('\n'),
-                short: true,
-            });
-        }
-
-        if (holidaysNext.length > 0) {
-            fields.push({
-                title: `Holidays ${ monday ? 'on Monday' : 'tomorrow' }:`,
-                value: holidaysNext.join('\n'),
-                short: true,
-            });
-        }
-
-        if (holidaysNextWeekend.length > 0) {
-            fields.push({
-                title: `Holidays in the weekend:`,
-                value: holidaysNextWeekend.join('\n'),
-                short: true,
-            });
-        }
-
-        if (fields.length === 0) {
+        if (attachments.length === 0) {
             return;
         }
 
@@ -158,7 +188,10 @@ export class Whosout {
         } else {
             messageBuilder.setRoom(this.app.zohoRoom);
         }
-        messageBuilder.addAttachment({ fields });
+
+        for (const attachment of attachments) {
+            messageBuilder.addAttachment(attachment);
+        }
         modify.getCreator().finish(messageBuilder);
     }
 }
